f0a1d0498caad084584ffa58f8609c974231f2b3
José Alejandro Sánchez Ortega <jasanchezo@gmail.com>
2018-12-20 22:52:06 -0600

    finalcommit finalcommit final

--- Json/ViewController.swift
+++ Json/ViewController.swift
@@ -1,25 +1,32 @@
-//
-//  ViewController.swift
-//  Json
-//
-//  Created by Freddy on 31/12/00.
-//  Copyright © 2000 Freddy. All rights reserved.
-//
-
+import WebKit
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var wkWebView: WKWebView!
+    var definicion:String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func buscar(_ sender: Any) {
+        definicion = textField.text!
+        let urlWiki = "https://es.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=\(definicion!)"
+        let urlObjeto = URL(string:urlWiki)
+        let tarea = URLSession.shared.dataTask(with: urlObjeto!) { (datos, respuesta, error) in
+            if error == nil {
+                do{
+                    let json = try JSONSerialization.jsonObject(with: datos!, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                    print(json)
+                }catch {
+                    print("El Procesamiento del JSON tuvo un error")
+                }
+            } else {
+                print(error!)
+            }
+        }
+        tarea.resume()
     }
-
-
 }
-
